836aaabedee4a7d5da1798fce5a06965f2bc636a
Shershan Mikalai <shershennick@gmail.com>
2015-10-19 16:17:54 +0300

    Initial commit

---
+++ CustomView.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace shershennm\seo;
+
+use Yii;
+use yii\web\View;
+
+/**
+* 
+*/
+class CustomView extends View
+{
+	public
+		$controllerNamespace;
+
+	private
+		$_controller;
+
+	public function getController()
+	{
+		if ($this->_controller === null)
+		{
+			$this->_controller = Yii::$app->controller;
+		}
+
+		return $this->_controller;
+	}
+	
+	public function init()
+	{
+		parent::init();
+		$this->on(self::EVENT_BEGIN_PAGE, [$this, 'eventSetMeta']);
+	}
+
+	public function eventSetMeta()
+	{
+		$controllerName = $this->buildControllerClass();
+		$actionName = $this->buildActionFunction();
+
+		if (class_exists($controllerName))
+		{
+			$object = new $controllerName;
+
+			if (method_exists($object, $actionName))
+			{
+				$object->controller = $this->controller;
+				$meta = $object->$actionName();
+
+				$this->addMeta($meta);
+			}
+		}
+	}
+
+	private function addMeta($metaArray)
+	{
+		foreach ($metaArray as $meta) {
+			$this->metaTags[] = Html::tag('meta', '', $meta);
+		}
+	}
+
+	private function buildControllerClass()
+	{
+		return sprintf('%s\%sController', $this->seoControllerNamespace, ucfirst($this->controller->id));
+	}
+
+	private function buildActionFunction()
+	{
+		return sprintf('action%s', ucfirst($this->controller->action->id));
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # yii2-seo
+
 Yii2 module for easy creating meta tags 
+
+# Usage
+in config file:
+```sh
+'components' => [
+	...
+    'view'         => [
+        'class' => 'shershennm/seo',
+        'controllerNamespace' => \\seo controllers namespace
+    ],
+    ...
+]
+```
+seo controller example:
+```sh
+class AppController extends Object
+{
+	public function actionIndex()
+	{
+		return [
+			['name' => 'keywords', 'value' => $this->getKeywords()],
+			['name' => 'description', 'value' => 'Cool page!'],
+		];
+	}
+
+	private function getKeywords()
+	{
+		return implode($this->controller->words, ', ');
+	}
+
+	....
+```
